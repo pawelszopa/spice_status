286a2b2b98b5d350184f766e9639fcdde3131eec
Pawel Szopa <expiren@gmail.com>
2020-12-17 17:22:21 +0100

    raw_data form added (total_client_req) DataRequired doesn't work on 0

--- spice_status/forms/workbook_form.py
+++ spice_status/forms/workbook_form.py
@@ -2,12 +2,12 @@ from datetime import datetime
 
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField, StringField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, DataRequired
 
 
 class WorkBookForm(FlaskForm):
     cw = StringField('Calendar Week leave empty to set current',
                      default=f'{datetime.now().isocalendar()[0]}CW{datetime.now().isocalendar()[1]}')
-    total_client_req = IntegerField('total_client_req', validators=[InputRequired()])
+    total_client_req = IntegerField('total_client_req', validators=[DataRequired()])
     total_client_req_approved = IntegerField('total_client_req_approved', validators=[InputRequired()])
     submit = SubmitField('Send')
